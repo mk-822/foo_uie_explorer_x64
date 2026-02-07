93d060aec7e353da620269840116f2958a87d842
ＭＫ <mst.kato@gmail.com>
2026-02-08 00:34:27 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Example panel
+
+https://columns-ui-sdk.readthedocs.io
+
+[![Build status](https://reupen.visualstudio.com/Columns%20UI/_apis/build/status/reupen.example_panel?branchName=master)](https://reupen.visualstudio.com/Columns%20UI/_build/latest?definitionId=4&branchName=master)
+
+Simple starter panel for developers of Columns UI panels.
+
+To get started with a new panel component:
+
+1. [Install Visual Studio 2019](https://visualstudio.microsoft.com/downloads/) with the 'Desktop development with C++' workload.
+2. [Install Git](https://git-scm.com/downloads).
+3. Clone this repo:
+   ```shell
+   git clone --recursive https://github.com/reupen/example_panel.git
+   ```
+4. Open `vc16/example_panel.sln`.
+5. Build the solution.
+6. If the build is successful, a component DLL will be created at `vc16\Debug\foo_uie_example.dll` or `vc16\Release\foo_uie_example.dll` (depending on the configuration selected).
+
+You can also click the 'Use this template' button above to start a new GitHub repository based on this one.
---
+++ foo_uie_example/main.cpp
@@ -0,0 +1,89 @@
+/**
+ * \file main.cpp
+ *
+ * \brief Example panel component
+ *
+ * This component is an example of a very simple multiple instance panel that does not take any keyboard input.
+ */
+
+// Included before windows.h, because pfc.h includes winsock2.h
+#include "../pfc/pfc.h"
+
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+
+#include "../foobar2000/SDK/foobar2000.h"
+
+#include "../columns_ui-sdk/ui_extension.h"
+
+/** Declare some information about our component */
+DECLARE_COMPONENT_VERSION("Example Columns UI Panel", "0.1", "compiled: " __DATE__);
+
+/**
+ * The GUID that identifies our panel.
+ *
+ * You must change this – don't use the same one!
+ */
+constexpr GUID extension_guid = {0x97ee6584, 0x7fb1, 0x48e9, {0xbf, 0xaa, 0xdc, 0xf3, 0x2, 0x95, 0x9a, 0xcf}};
+
+/** Our window class. */
+class ExampleWindow : public uie::container_ui_extension {
+public:
+    const GUID& get_extension_guid() const override { return extension_guid; }
+    void get_name(pfc::string_base& out) const override { out = "Example"; }
+    void get_category(pfc::string_base& out) const override { out = "Panels"; }
+    unsigned get_type() const override { return uie::type_panel; }
+
+private:
+    class_data& get_class_data() const override
+    {
+        __implement_get_class_data(_T("{97EE6584-7FB1-48E9-BFAA-DCF302959ACF}"), true);
+    }
+
+    /** Our window procedure */
+    LRESULT on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp) override;
+    void get_menu_items(uie::menu_hook_t& p_hook) override;
+
+    /** Our child window */
+    HWND wnd_static{nullptr};
+};
+
+void ExampleWindow::get_menu_items(uie::menu_hook_t& p_hook)
+{
+    const uie::menu_node_ptr close_panel = new uie::simple_command_menu_node(
+        "Close panel", "Removes this panel from the layout.", 0, [self = service_ptr_t<ExampleWindow>(this)] {
+            const HWND wnd = self->get_wnd();
+            uie::window_host_ptr host = self->get_host();
+            uie::window_ptr(self)->destroy_window();
+            host->relinquish_ownership(wnd);
+        });
+    p_hook.add_node(close_panel);
+}
+
+LRESULT ExampleWindow::on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg) {
+    case WM_CREATE: {
+        RECT rc;
+        GetClientRect(wnd, &rc);
+
+        /** Create a static window, with text "Example". */
+        wnd_static = CreateWindowEx(0, WC_STATIC, _T("Example panel"), WS_CHILD | WS_VISIBLE, 0, 0, rc.right, rc.bottom,
+            wnd, HMENU(0), core_api::get_my_instance(), nullptr);
+    } break;
+    case WM_SIZE:
+        /** The static control requires this to redraw correctly. */
+        RedrawWindow(wnd_static, 0, 0, RDW_INVALIDATE | RDW_ERASE);
+        /** Reposition the static control. */
+        SetWindowPos(wnd_static, 0, 0, 0, LOWORD(lp), HIWORD(lp), SWP_NOZORDER);
+        break;
+    case WM_DESTROY:
+        /** DefWindowProc will destroy our child window. Set our window handle to nullptr now. */
+        wnd_static = nullptr;
+        break;
+    }
+    return DefWindowProc(wnd, msg, wp, lp);
+}
+
+static uie::window_factory<ExampleWindow> example_window_factory;
